19f0b27cf4a01ba2686700e19248216dc2be18f3
Агатін Євген <65392476+Jackrir@users.noreply.github.com>
2021-07-05 16:55:43 +0300

    Create simple BalancedBrackets

---
+++ AkvelonTask/BalanceVerificator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkvelonTask
+{
+    public class BalanceVerificator
+    {
+        private int GradationRoundBracket {get;set;}
+        private int LastOpenRoundBracket { get; set; }
+        private int GradationBraces { get; set; }
+        private int LastOpenBraces { get; set; }
+        private int GradationSquareBracket { get; set; }
+        private int LastOpenSquareBracket { get; set; }
+
+        public BalanceVerificator()
+        {
+            GradationRoundBracket = 0;
+            GradationBraces = 0;
+            GradationSquareBracket = 0;
+            LastOpenRoundBracket = -1;
+            LastOpenBraces = -1;
+            LastOpenSquareBracket = -1;
+        }
+
+        public int IsBalancedBracketsString(string bracketsString)
+        {
+            int i = 0;
+            foreach(char bracket in bracketsString)
+            {
+                if (!UpdateGradationData(bracket, i))
+                    return -2;
+                int validationGradationCloseBracketResult = isValidGradationCloseBracket(i);
+                if (validationGradationCloseBracketResult != -1)
+                    return validationGradationCloseBracketResult;
+                i++;
+            }
+            int validationGradationOpenBracketResult = isValidGradationOpenBracket();
+            if (validationGradationOpenBracketResult != -1)
+                return validationGradationOpenBracketResult;
+            return -1;
+
+        }
+
+        private bool UpdateGradationData(char bracket, int index)
+        {
+            switch (bracket)
+            {
+                case '(':
+                    GradationRoundBracket++;
+                    LastOpenRoundBracket = index;
+                    return true;
+                case ')':
+                    GradationRoundBracket--;
+                    return true;
+                case '{':
+                    GradationBraces++;
+                    LastOpenBraces = index;
+                    return true;
+                case '}':
+                    GradationBraces--;
+                    return true;
+                case '[':
+                    GradationSquareBracket++;
+                    LastOpenSquareBracket = index;
+                    return true;
+                case ']':
+                    GradationSquareBracket--;
+                    return true;
+                default:
+                    Console.WriteLine("A character ‘{0}’ doesn’t belong to any known brackets type, returns IllegalArgumentException.", bracket);
+                    return false;
+            }
+        }
+
+        private int isValidGradationCloseBracket(int i)
+        {
+            if (GradationBraces < 0 || GradationRoundBracket < 0 || GradationSquareBracket < 0)
+            {
+                return i;
+            }
+            return -1;
+        }
+
+        private int isValidGradationOpenBracket()
+        {
+            if (GradationBraces > 0 || GradationRoundBracket > 0 || GradationSquareBracket > 0)
+            {
+                if (GradationRoundBracket > 0)
+                    return LastOpenRoundBracket;
+                else if (GradationBraces > 0)
+                    return LastOpenBraces;
+                else
+                    return LastOpenSquareBracket;
+            }
+            return -1;
+        }
+    }
+}
--- AkvelonTask/Program.cs
+++ AkvelonTask/Program.cs
@@ -6,7 +6,30 @@ namespace AkvelonTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BalanceVerificator verificator = new BalanceVerificator();
+            Console.WriteLine("Write brackets string:");
+            while(true)
+            {
+                string bracketsString = Console.ReadLine();
+                PrintResult(verificator.IsBalancedBracketsString(bracketsString));
+                verificator = new BalanceVerificator();
+            }
         }
+
+        static void PrintResult(int result)
+        {
+            switch (result)
+            {
+                case -2:
+                    break;
+                case -1:
+                    Console.WriteLine("BALANCED, returns -1");
+                    break;
+                default:
+                    Console.WriteLine("NOT BALANCED ({0}), returns {0}", result + 1);
+                    break;
+            }
+        }
+
     }
 }
